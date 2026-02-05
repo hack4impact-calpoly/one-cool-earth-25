a4d4cdb06ff94b96b383b21d572ca90c87c60b7f
Amogh76 <amogharora06@gmail.com>
2026-02-04 17:34:06 -0800

    frontend sign up page

---
+++ src/app/create-account/page.tsx
@@ -0,0 +1,9 @@
+import CreateAccountForm from "@/components/CreateAccountForm";
+
+export default function CreateAccountPage() {
+  return (
+    <div className="min-h-screen bg-white flex justify-center">
+      <CreateAccountForm />
+    </div>
+  );
+}
