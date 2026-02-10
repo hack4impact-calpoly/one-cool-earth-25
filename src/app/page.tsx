4d5011ace03a9f6974d6be34e0bbd217a09a2c70
Messiah Afzalyar <messitube38@gmail.com>
2026-02-10 13:24:46 -0800

    Removed component from page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Navbar from "@/components/Navbar";
-import VolunteerList from "@/components/VolunteerList";
 
 export default function Home() {
   return (
     <main style={{ height: "100vh", padding: 20 }}>
       <Navbar />
       <h1>Home</h1>
-      <VolunteerList />
     </main>
   );
 }
