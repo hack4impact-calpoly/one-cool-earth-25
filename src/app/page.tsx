094f142afa6d1e1e916484bb3b6844ff3bef1c9c
Ay-355 <77558710+Ay-355@users.noreply.github.com>
2026-02-22 00:12:51 -0800

    fix: remove navbar component from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,5 @@
-import Navbar from "@/components/Navbar";
 import LoginPage from "./login/page";
 
 export default function Home() {
-  return (
-    <main style={{ height: "100vh", padding: 20 }}>
-      <Navbar />
-    </main>
-  );
+  return <main style={{ height: "100vh", padding: 20 }}></main>;
 }
