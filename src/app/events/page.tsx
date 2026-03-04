ad63928f5eda76b0376871707f102d7f250bbaa5
lheutchy <lheutchy@gmail.com>
2026-03-03 16:46:18 -0800

    feat: create Volunteer Events Page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,31 +1,9 @@
-// app/(main)/events/[id]/page.tsx
-import Image from "next/image";
-import styles from "@/styles/events.module.css";
+import VolunteerEventsPage from "@/components/VolunteerEventsPage";
 
-// ✅ Update these imports to your actual component paths
-import EventDetailsCard from "@/components/EventDetails";
-import VolunteerList from "@/components/VolunteerList";
-import NavBar from "@/components/Navbar";
-
-export default function AdminEventPage() {
-  // Later you can fetch event by params.id and pass data into the components.
+export default function CreateEventsPage() {
   return (
-    <div className={styles.page}>
-      <NavBar mode="Admin" />
-      <main className={styles.main}>
-        <h1 className={styles.pageTitle}>Garden Workday</h1>
-
-        <section className={styles.grid}>
-          {/* Left card: Event Details */}
-          <div className={styles.eventCard}>
-            <EventDetailsCard />
-          </div>
-          {/* Right card: Volunteers */}
-          <div className={styles.card}>
-            <VolunteerList />
-          </div>
-        </section>
-      </main>
+    <div className="min-h-screen bg-white flex justify-center">
+      <VolunteerEventsPage />
     </div>
   );
 }
