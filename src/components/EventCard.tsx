90a4cdcbd77d9bd5cce19cd66242e0486f01eab4
jojayama <jodiyamane27@gmail.com>
2026-03-05 01:35:27 -0800

    fix: changed navar so that it has differences between volunteer and admin; added admin event details to admin calendar on 'learn more' button

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -1,4 +1,6 @@
+"use client";
 import { Button } from "@mui/material";
+import { useRouter } from "next/navigation";
 
 interface EventProps {
   eventTitle: string;
@@ -8,6 +10,7 @@ interface EventProps {
 export default function EventCard({ eventTitle, date }: EventProps) {
   const datePart = date.toLocaleDateString("en-US", { month: "long", day: "numeric" });
   const timePart = date.toLocaleTimeString("en-US", { hour: "numeric", minute: "2-digit" });
+  const router = useRouter();
 
   // Format the date into a string like "Nov 10, 11:00 AM" (exact format depends on the locale)
   const formattedDateTime: string = `${datePart}, ${timePart}`;
@@ -15,7 +18,10 @@ export default function EventCard({ eventTitle, date }: EventProps) {
     <div className="flex flex-col items-center space-y-2 w-42 shrink-0 m-4 p-4 bg-[#fcfce6] rounded-xl">
       <h1 className="text-2xl mb-1 font-bold">{eventTitle}</h1>
       <div className="text-lg">{formattedDateTime}</div>
-      <button className="bg-[#448EBF] bg-opacity-20 m-1 p-2">Learn More</button>
+      <button onClick={() => router.push(`/admin-event-details`)} className="bg-[#448EBF] bg-opacity-20 m-1 p-2">
+        Learn More
+      </button>
+      {/* will need to get rid of event-details hardcode navigation push and instead use the id of the event to navigate to the correct event details */}
       <button className="bg-[#226999] m-1 p-2 text-white">Register!</button>
     </div>
   );
